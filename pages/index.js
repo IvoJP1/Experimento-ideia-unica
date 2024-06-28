34286aa02775bc62a7c3b68b4038453d1a02b631
IvoJP <88736020+IvoJP1@users.noreply.github.com>
2024-06-28 11:19:17 -0300

    atualização teste para deploy automatico

--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,10 @@ function Home(){
             <h1>
                 Home
             </h1>
-            <Contador/>
+          
         </div>
     )
 }
 
-function Contador(){
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-
-    return(
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )  
-}
 
 export default Home
